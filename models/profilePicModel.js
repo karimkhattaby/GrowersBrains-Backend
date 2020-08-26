4bb8d57c8eaa93b8ca32ee66d2901530facd64f2
Atae Edrissi <ataeedrissi@gmail.com>
2020-08-26 03:40:57 +0100

    Add reference to the owner of profile picture

--- models/profilePicModel.js
+++ models/profilePicModel.js
@@ -2,21 +2,24 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const profilePicSchema = new Schema({
-    fileName: {
-        required: true,
-        type: String,
-    },
-    owner: {
-        required: true,
-        type: String
-    },
-    createdAt: {
-        default: Date.now(),
-        type: Date,
-    },
-    profilePicId: {
-        type: Schema.Types.ObjectId
-    }
+  fileName: {
+    required: true,
+    type: String,
+    unique: true,
+  },
+  owner: {
+    type: mongoose.Schema.ObjectId,
+    required: true,
+    unique: true,
+    ref: 'User',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  //   profilePicId: {
+  //     type: Schema.Types.ObjectId,
+  //   },
 });
 
 const ProfilePic = mongoose.model('ProfilePic', profilePicSchema);
